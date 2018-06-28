43d0765eb95f834fd6f7760d95c1afa7bba2a40a
Mario Lema <lema_mario@yahoo.com>
2018-06-28 12:27:01 +0100

    add fetch

--- mars_weather.js
+++ mars_weather.js
@@ -1,11 +1,61 @@
-const weatherFacts =[
-	"Mercury almost completely lacks an atmosphere. As the closest planet to the sun, it's no surprise that the planet can get extremely hot. However, the lack of atmosphere means that it is unable to retain the heat and as a result, it exhibits incredible temperature fluctuations. Pack for summer AND winter.",
-	"Rain on Venus is almost purely sulfuric acid, which is extremely corrosive. Sulfuric acid can erode clothing nearly instantly and produce severe burns on flesh. However, the surface temperature of Venus is so great, the rain evaporates before hitting the ground.",
-	"Jupiter has dozens of jet streams, ammonia clouds, and hurricane-like storms large enough to swallow the Earth multiple times over.",
-	"The surface of Europa is covered in a 100 km deep (62 mile) saltwater ocean, which is enclosed in a layer of ice. Europa may even have some of the chemical compounds needed for life.",
-	"Titan, Saturn’s largest moon, has huge lakes where the temperature is about -162ºC (-260ºF), and the lakes aren’t made of water - they're actually liquid methane!",
-	"Uranus is the coldest planet in the solar system, with temperatures hitting −224ºC (-371ºF).Like Jupiter, Uranus also has diamonds raining down on its surface.",
-	"Like Jupiter, Saturn’s atmosphere is composed mostly of hydrogen. Wind speeds can reach as high as 1609 km/h (1000 mph) which is just about as fast as a speeding bullet.",
-	"A trip to Neptune would include listening to the sound barrier break as the wind blows, though the visitor would freeze solid almost instantly."
-];
+let state = {
+	sol: '--',
+	month: '--',
+	maxC: '--',
+	minC: '--',
+	opacity: '--',
+	sunset: '--',
+	sunrise: '--',
+};
+
+const view = {
+	selectors: {	
+	sol: document.querySelector('.sol'),
+	month: document.querySelector('.season'),
+	temperature: document.querySelector('.temperature'),
+	opacity: document.querySelector('.atmo'),
+	sunTime: document.querySelector('.sunTime')
+	},
+
+	updateView: function(){
+		this.selectors.sol.innerText = `SOL: ${state.sol}`;
+		this.selectors.month.innerText = `MONTH: ${state.month}`;
+		this.selectors.temperature.innerText = `MIN \xB0C: ${state.minC}\nMAX \xB0C: ${state.maxC}`;
+		this.selectors.opacity.innerText = `OPACITY: ${state.opacity}`;
+		this.selectors.sunTime.innerText = `SUNRISE: ${state.sunrise}\nSUNSET: ${state.sunset}`;
+	}
+};
+
+
+const processor = {
+	APIcall: function(){
+		fetch('https://api.maas2.jiinxt.com/')
+  			.then(function(response) {
+  				if(response.ok){
+  					return response.json();
+  				};
+  			})
+  			.then(function(data) {
+  				state = {
+  				sol: String(data.sol),
+				month: data.season.split(' ')[1],
+				maxC: `${data.max_temp}\xB0`,
+				minC: `${data.min_temp}\xB0`,
+				opacity: data.atmo_opacity,
+				sunset: data.sunset,
+				sunrise: data.sunrise,
+  			};
+  			view.updateView();
+  			});
+	}
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+	processor.APIcall();
+	view.updateView();
+})
+
+
+
+
 
